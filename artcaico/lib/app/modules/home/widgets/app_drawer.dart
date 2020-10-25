410d3c84df24dc1ffa2ef84a5c5c9ea7e456f0a3
Jose Flavio da Silva Maia <makarovmaia@gmail.com>
2020-10-25 10:02:25 -0300

    Criando pasta widgets para widgets especificos.

---
+++ artcaico/lib/app/modules/home/widgets/app_drawer.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+class AppDrawer extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      child: Column(
+        children: [
+          AppBar(
+            title: Text('Bem vindo Usuário!'),
+            automaticallyImplyLeading: false,
+          ),
+          Divider(),
+          ListTile(
+            leading: Icon(Icons.shop),
+            title: Text('Loja'),
+            onTap: () {},
+          ),
+          Divider(),
+          ListTile(
+            leading: Icon(Icons.payment),
+            title: Text('Pedidos'),
+            onTap: () {},
+          ),
+          Divider(),
+          ListTile(
+            leading: Icon(Icons.edit),
+            title: Text('Gerenciar Produtos'),
+            onTap: () {},
+          ),
+        ],
+      ),
+    );
+  }
+}
